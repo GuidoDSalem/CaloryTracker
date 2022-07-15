d9b09ae806cabb184ad1f552c5b4c0fe4cf9ad00
GuidoDSalem <guidosalem.dev@gmail.com>
2022-07-15 03:09:09 -0300

    Welcome Screen and core-ui

--- tracker/tracker_presentation/build.gradle.kts
+++ tracker/tracker_presentation/build.gradle.kts
@@ -6,6 +6,7 @@ apply {
 dependencies {
     "implementation"(project(Modules.core))
     "implementation"(project(Modules.trackerDomain))
+    "implementation"(project(Modules.coreUi))
 
     "implementation"(Coil.coilCompose)
 }
